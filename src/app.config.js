c805f12f934229dcd2d7819d072512c9df0123a5
kpepruthvi <kpavaneswerpruthv@deloitte.com>
2022-09-01 06:59:18 +0000

    Team name modified

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Team3';
+    team = 'Team-';
     version = '1.0.0';
 }
 module.exports = AppConfig;
